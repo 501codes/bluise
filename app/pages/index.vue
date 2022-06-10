c7cea1058c4e39a82ec4f4acab629ee7c03df536
501codes <rhafener@outlook.com>
2022-06-09 23:19:50 -0400

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
-      <div class="flex flex-col w-full xl:w-3/5">
+      <div class="flex flex-col w-full xl:w-5/5">
         <img
           alt="Playing Tennis"
           class="rounded shadow-xl"
